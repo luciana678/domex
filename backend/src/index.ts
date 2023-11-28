73fe5d4c82a96aa9bee77d17f6de585f9faa7bda
David Scoffield <davidscoffield01@gmail.com>
2023-11-28 13:06:14 -0300

    back forward

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,11 @@ import express, { type Request, type Response } from 'express'
 
 const app = express()
 const PORT = 3000
+let a = 1
+
+console.log(a)
+
+a = 'a'
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Helloo, TypeScript with Express!')
