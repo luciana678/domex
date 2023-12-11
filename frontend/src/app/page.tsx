0b1ae3bfad809857ccb5450a08311b63a9324675
David Scoffield <davidscoffield01@gmail.com>
2023-12-11 16:54:52 -0300

    Format files and add commands

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,19 +1,24 @@
-import { Button, TextField } from "@mui/material";
+import { Button, TextField } from '@mui/material'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <div className="pb-5">
-      <TextField label="Usuario"></TextField>
+    <main className='flex min-h-screen flex-col items-center p-24'>
+      <div className='pb-5'>
+        <TextField label='Usuario'></TextField>
       </div>
-      <Button sx={{
-        width: 225,
-        height: 50,
-        marginBottom: 2.5
-      }} variant="outlined" color="success">Crear un cluster</Button>
-      <div className="flex gap-5 mb-5">
-      <TextField label="Unirse a un cluster"></TextField>
-      <Button variant="outlined">Unirse</Button>
+      <Button
+        sx={{
+          width: 225,
+          height: 50,
+          marginBottom: 2.5,
+        }}
+        variant='outlined'
+        color='success'>
+        Crear un cluster
+      </Button>
+      <div className='flex gap-5 mb-5'>
+        <TextField label='Unirse a un cluster'></TextField>
+        <Button variant='outlined'>Unirse</Button>
       </div>
     </main>
   )
