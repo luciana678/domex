477c11bbd5d28a27a7ec2025b2fd84f015db1b18
David Scoffield <davidscoffield01@gmail.com>
2023-12-13 15:08:09 -0300

    Add types of Props  to BasicAccordion

--- frontend/src/components/Accordion.tsx
+++ frontend/src/components/Accordion.tsx
@@ -1,41 +1,37 @@
-import * as React from "react";
-import Accordion from "@mui/material/Accordion";
-import AccordionSummary from "@mui/material/AccordionSummary";
-import AccordionDetails from "@mui/material/AccordionDetails";
-import Typography from "@mui/material/Typography";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import { Button } from "@mui/material";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import { Button } from '@mui/material'
+import Accordion from '@mui/material/Accordion'
+import AccordionDetails from '@mui/material/AccordionDetails'
+import AccordionSummary from '@mui/material/AccordionSummary'
+import Typography from '@mui/material/Typography'
 
-export default function BasicAccordion({ titulo }) {
+export default function BasicAccordion({ titulo }: { titulo: string }) {
   return (
     <Accordion
       sx={{
-        width: "100%",
+        width: '100%',
         mb: 2.5,
-      }}
-    >
+      }}>
       <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-        <div className="flex w-full">
+        <div className='flex w-full'>
           <Typography
             sx={{
               flexGrow: 1,
-            }}
-          >
+            }}>
             {titulo}
           </Typography>
           <Button
             sx={{
               ml: 5,
             }}
-            variant="outlined"
-          >
+            variant='outlined'>
             Cargar archivo
           </Button>
         </div>
       </AccordionSummary>
-      <AccordionDetails className="w-full">
+      <AccordionDetails className='w-full'>
         <textarea></textarea>
       </AccordionDetails>
     </Accordion>
-  );
+  )
 }
