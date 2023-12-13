d0f23a6674750eb7abe4300f6867f9c8cd1af631
David Scoffield <davidscoffield01@gmail.com>
2023-12-13 15:00:20 -0300

    Add readonly prop to CodeEditor

--- frontend/src/components/CodeEditor.tsx
+++ frontend/src/components/CodeEditor.tsx
@@ -2,19 +2,25 @@
 
 import { Editor, EditorProps } from '@monaco-editor/react'
 
+type Props = EditorProps & {
+  readOnly?: boolean
+}
+
 export default function CodeEditor({
   height = '100%',
   defaultLanguage = 'python',
   defaultValue = '// write your code here',
   theme = 'vs-dark',
+  readOnly = false,
   ...editorProps
-}: EditorProps) {
+}: Props) {
   return (
     <Editor
       height={height}
       defaultLanguage={defaultLanguage}
       defaultValue={defaultValue}
       theme={theme}
+      options={{ readOnly }}
       {...editorProps}
     />
   )
