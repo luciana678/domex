9c80212f91fe498dd065685cb1539dc0ec9eceb6
David Scoffield <davidscoffield01@gmail.com>
2023-12-23 19:34:28 -0300

    Refactor useRooms

--- frontend/src/hooks/useRoom.ts
+++ frontend/src/hooks/useRoom.ts
@@ -24,84 +24,75 @@ const useRoom = () => {
   }, [router])
 
   useEffect(() => {
-    socket.on(
-      'room:session',
-      ({
-        sessionID,
-        userID,
-        roomID,
-        userName,
-      }: {
-        sessionID: SessionID
-        userID: UserID
-        roomID: RoomID
-        userName: string
-      }) => {
-        const session: RoomSession = { sessionID, roomID, userName }
-        setRoomSession(session)
-        // attach the session ID to the next reconnection attempts
-        socket.auth = session
-        // store it in the sessionStorage
-        sessionStorage.setItem('session', JSON.stringify(session))
-        // save the ID of the user
-        socket.userID = userID
-        // go to the room
-        router.push(`/room/${roomID}`)
-      },
-    )
+    const onSession = ({
+      sessionID,
+      userID,
+      roomID,
+      userName,
+    }: {
+      sessionID: SessionID
+      userID: UserID
+      roomID: RoomID
+      userName: string
+    }) => {
+      const session: RoomSession = { sessionID, roomID, userName }
+      setRoomSession(session)
+      // attach the session ID to the next reconnection attempts
+      socket.auth = session
+      // store it in the sessionStorage
+      sessionStorage.setItem('session', JSON.stringify(session))
+      // save the ID of the user
+      socket.userID = userID
+      // go to the room
+      router.push(`/room/${roomID}`)
+    }
 
-    socket.on('room:users', (users: User[]) => {
+    const onUsers = (users: User[]) => {
       setClusterUsers(users)
-    })
+    }
 
-    socket.on('room:user-leave', ({ userID, userName }: { userID: UserID; userName: string }) => {
+    const onUserLeave = ({ userID, userName }: { userID: UserID; userName: string }) => {
       // A user has left, remove it from the list
       setClusterUsers((prevUsers) => prevUsers.filter((user) => user.userID !== userID))
-    })
+    }
 
-    socket.on(
-      'room:user-disconnected',
-      ({ userID, userName }: { userID: UserID; userName: string }) => {
-        // A user has disconnected, update the connected status
+    const onUserDisconnected = ({ userID, userName }: { userID: UserID; userName: string }) => {
+      // A user has disconnected, update the connected status
+      setClusterUsers((prevUsers) =>
+        prevUsers.map((user) => {
+          if (user.userID === userID) {
+            return { ...user, connected: false }
+          }
+          return user
+        }),
+      )
+    }
+
+    const onUserConnected = ({
+      userID,
+      userName,
+      connected,
+    }: {
+      userID: UserID
+      userName: string
+      connected: boolean
+    }) => {
+      // If the user is already in the list, update the connected status, otherwise add it to the list
+      if (clusterUsers.some((user) => user.userID === userID)) {
         setClusterUsers((prevUsers) =>
           prevUsers.map((user) => {
             if (user.userID === userID) {
-              return { ...user, connected: false }
+              return { ...user, connected }
             }
             return user
           }),
         )
-      },
-    )
+      } else {
+        setClusterUsers((prevUsers) => [...prevUsers, { userID, userName, connected }])
+      }
+    }
 
-    socket.on(
-      'room:user-connected',
-      ({
-        userID,
-        userName,
-        connected,
-      }: {
-        userID: UserID
-        userName: string
-        connected: boolean
-      }) => {
-        // If the user is already in the list, update the connected status, otherwise add it to the list
-        if (clusterUsers.some((user) => user.userID === userID)) {
-          setClusterUsers((prevUsers) =>
-            prevUsers.map((user) => {
-              if (user.userID === userID) {
-                return { ...user, connected }
-              }
-              return user
-            }),
-          )
-        } else {
-          setClusterUsers((prevUsers) => [...prevUsers, { userID, userName, connected }])
-        }
-      },
-    )
-
-    socket.on('connect_error', (err) => {
+    const onConnectError = (err: Error) => {
       if (err.message === 'missing username') {
         console.error('missing username')
         sessionStorage.removeItem('session')
@@ -110,15 +101,22 @@ const useRoom = () => {
           router.push('/')
         }
       }
-    })
+    }
+
+    socket.on('room:session', onSession)
+    socket.on('room:users', onUsers)
+    socket.on('room:user-leave', onUserLeave)
+    socket.on('room:user-disconnected', onUserDisconnected)
+    socket.on('room:user-connected', onUserConnected)
+    socket.on('connect_error', onConnectError)
 
     return () => {
-      socket.off('room:session')
-      socket.off('room:users')
-      socket.off('connect_error')
-      socket.off('room:user-leave')
-      socket.off('room:user-disconnected')
-      socket.off('room:user-connected')
+      socket.off('room:session', onSession)
+      socket.off('room:users', onUsers)
+      socket.off('room:user-leave', onUserLeave)
+      socket.off('room:user-disconnected', onUserDisconnected)
+      socket.off('room:user-connected', onUserConnected)
+      socket.off('connect_error', onConnectError)
     }
   }, [clusterUsers, pathname, router, setClusterUsers, setRoomSession])
 
