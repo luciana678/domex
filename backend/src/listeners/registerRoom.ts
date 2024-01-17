9461b1c57055352e63563cf2ebea6a1de230a0ea
David Scoffield <davidscoffield01@gmail.com>
2024-01-16 21:29:30 -0300

    Turn back to non separate logic for owner connection

--- backend/src/listeners/registerRoom.ts
+++ backend/src/listeners/registerRoom.ts
@@ -1,7 +1,6 @@
 import { type Server, type Socket } from 'socket.io'
 import LoggerService from '../services/logger.services.js'
 import { type InMemoryRoomSessionStore } from '../store/InMemorySessionStore.js'
-import { type Session } from '../types.js'
 
 export const registerRoom = async (
   io: Server,
@@ -30,40 +29,41 @@ export const registerRoom = async (
   // join the user's own room
   await socket.join(socket.userID)
 
-  const fetchExistingUsers = (socket: Socket): Session[] => {
-    const users = roomsSessionStore.findAllSessions(socket.roomID)
-    return users.filter((user) => user.userID !== socket.userID)
-  }
+  const users = roomsSessionStore.findAllSessions(socket.roomID)
+  const usersWitoutMe = users.filter((user) => user.userID !== socket.userID)
+  socket.emit('room:users', usersWitoutMe)
 
-  const emitRoomUsers = (socket: Socket, event: string, users: Session[]): void => {
-    socket.emit('room:users', users)
+  socket.broadcast.to(socket.roomID).emit(`room:user-connected`, {
+    userID: socket.userID,
+    userName: socket.userName,
+    isRoomOwner: socket.isRoomOwner,
+    connected: true,
+  })
 
-    socket.broadcast.to(socket.roomID).emit(`room:${event}`, {
-      userID: socket.userID,
-      userName: socket.userName,
-      connected: true,
-    })
-  }
-
-  const handleLeaveRoom = async (
-    socket: Socket,
-    eventType: string,
-    logMessage: string,
-  ): Promise<void> => {
-    LoggerService.socket(logMessage)
+  socket.on('room:leave-room', async () => {
+    LoggerService.socket(
+      `${socket.isRoomOwner ? 'Owner' : 'User'} ${socket.userName} left the room: ${
+        socket.roomID
+      }, ${socket.userID}`,
+    )
     roomsSessionStore.removeSession(socket.roomID, socket.sessionID)
 
-    socket.broadcast.to(socket.roomID).emit(`room:${eventType}`, {
+    socket.broadcast.to(socket.roomID).emit(`room:user-leave`, {
       userID: socket.userID,
       userName: socket.userName,
     })
 
     await socket.leave(socket.roomID)
     await socket.leave(socket.userID)
-  }
+  })
 
-  const handleDisconnect = (socket: Socket, eventType: string, logMessage: string): void => {
-    LoggerService.socket(logMessage, socket.id)
+  socket.on('disconnect', () => {
+    LoggerService.socket(
+      `${socket.isRoomOwner ? 'Owner' : 'User'} ${socket.userName} disconnected from room: ${
+        socket.roomID
+      }`,
+      socket.id,
+    )
 
     const session = roomsSessionStore.findSession(socket.roomID, socket.sessionID)
     if (!session) return
@@ -71,54 +71,13 @@ export const registerRoom = async (
     roomsSessionStore.saveSession(socket.roomID, socket.sessionID, {
       userName: socket.userName,
       userID: socket.userID,
-      connected: false,
       isRoomOwner: socket.isRoomOwner,
+      connected: false,
     })
 
-    socket.broadcast.to(socket.roomID).emit(`room:${eventType}`, {
+    socket.broadcast.to(socket.roomID).emit(`room:user-disconnected`, {
       userID: socket.userID,
       userName: socket.userName,
     })
-  }
-
-  if (socket.isRoomOwner) {
-    const users = fetchExistingUsers(socket)
-    emitRoomUsers(socket, 'owner-connected', users)
-
-    socket.on('room:leave-room', async (): Promise<void> => {
-      await handleLeaveRoom(
-        socket,
-        'owner-leave',
-        `The owner ${socket.userName} left the room: ${socket.roomID}, ${socket.userID}`,
-      )
-    })
-
-    socket.on('disconnect', (): void => {
-      handleDisconnect(
-        socket,
-        'owner-disconnected',
-        `Owner ${socket.userName} disconnected from room: ${socket.roomID}`,
-      )
-    })
-  } else {
-    const users = fetchExistingUsers(socket)
-    // .filter((user) => !user.isRoomOwner)
-    emitRoomUsers(socket, 'user-connected', users)
-
-    socket.on('room:leave-room', async () => {
-      await handleLeaveRoom(
-        socket,
-        'user-leave',
-        `User ${socket.userName} left the room: ${socket.roomID}, ${socket.userID}`,
-      )
-    })
-
-    socket.on('disconnect', () => {
-      handleDisconnect(
-        socket,
-        'user-disconnected',
-        `User ${socket.userName} disconnected from room: ${socket.roomID}`,
-      )
-    })
-  }
+  })
 }
