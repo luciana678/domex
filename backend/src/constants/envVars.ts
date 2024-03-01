f7a646efaf208f1c8c5b744d54ae8f5e9e4117e0
David Scoffield <davidscoffield01@gmail.com>
2024-02-29 21:35:40 -0300

    Fix HTTPS envVar

--- backend/src/constants/envVars.ts
+++ backend/src/constants/envVars.ts
@@ -1,6 +1,6 @@
 export const NODE_ENV = process.env.NODE_ENV ?? 'development'
 export const PORT = Number(process.env.PORT) || 5000
 export const HOST = process.env.HOST ?? 'localhost'
-export const HTTPS = Boolean(process.env.HTTPS) || false
+export const HTTPS = process.env.HTTPS === 'true'
 export const SERVER_KEY_NAME = process.env.SERVER_KEY_NAME ?? 'server.key'
 export const SERVER_CERT_NAME = process.env.SERVER_CERT_NAME ?? 'server.crt'
