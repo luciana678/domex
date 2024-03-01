e020fa0133cd221e23008f6b4f50d47be356c5b7
David Scoffield <davidscoffield01@gmail.com>
2024-03-01 01:22:02 -0300

    Refactor socket initialization to support path-rewriting proxy

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,9 +1,26 @@
-import { io } from 'socket.io-client'
+import { type ManagerOptions, type SocketOptions, io } from 'socket.io-client'
 import { ENVS } from './constants/envs'
 
-export const socket = io(ENVS.SERVER.URL, {
+// Check if the url is using a path-rewriting proxy in between tecniche
+// If so, we need to use the proxy url instead of the server url
+// Descompose the url to check if it has a path
+const url = new URL(ENVS.SERVER.URL)
+const path = url.pathname
+const socketURL = url.origin
+
+let options: Partial<ManagerOptions & SocketOptions> = {
   autoConnect: false,
-})
+}
+
+if (path !== '/') {
+  // If the url has a path, we need to add it to the socket options
+  options = {
+    ...options,
+    path: path + (path.endsWith('/') ? 'socket.io' : '/socket.io'),
+  }
+}
+
+export const socket = io(socketURL, options)
 
 socket.onAny((event, ...args) => {
   console.log('---', event, args)
