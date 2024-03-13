7327fa298f41bda0c17c519df1d1b2e26b2e26ca
David Scoffield <davidscoffield01@gmail.com>
2024-03-13 15:10:33 -0300

    Fix bug in depts loop

--- frontend/src/components/Slave.tsx
+++ frontend/src/components/Slave.tsx
@@ -3,21 +3,21 @@
 import InputSelector from '@/components/InputSelector'
 import { Statistics } from '@/components/Statistics'
 import { placeholdersFunctions } from '@/constants/functionCodes'
+import { initialSizes } from '@/context/MapReduceContext'
+import useMapReduce from '@/hooks/useMapReduce'
 import usePeers from '@/hooks/usePeers'
 import useRoom from '@/hooks/useRoom'
-import { KeyValues, MapCombinerResults, KeyValue, UserID, Sizes, FinalResults } from '@/types'
+import useStatistics from '@/hooks/useStatisticts'
+import { FinalResults, KeyValue, KeyValues, MapCombinerResults, Sizes, UserID } from '@/types'
+import { concatenateFiles } from '@/utils/helpers'
 import { PY_MAIN_CODE } from '@/utils/python/tmp'
 import { Button } from '@mui/material'
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { usePython } from 'react-py'
 import BasicAccordion from './Accordion'
 import Navbar from './Navbar'
 import NodeList from './NodeList'
-import useMapReduce from '@/hooks/useMapReduce'
-import { initialSizes } from '@/context/MapReduceContext'
 import Results from './Results'
-import { concatenateFiles } from '@/utils/helpers'
-import useStatistics from '@/hooks/useStatisticts'
 
 const initialMapCombinerResults: MapCombinerResults = {
   mapResults: {},
@@ -31,28 +31,21 @@ export default function Slave() {
   const [selectedFiles, setSelectedFiles] = useState<File[]>([])
   const [mapCombinerResults, setMapCombinerResults] =
     useState<MapCombinerResults>(initialMapCombinerResults)
-  const mapCombinerExecuted =
-    selectedFiles.length > 0 ? Object.keys(mapCombinerResults.mapResults).length > 0 : true
-  const combinerResults = mapCombinerResults.combinerResults
-  const [isReadyToExecute, setIsReadyToExecute] = useState(false)
   const [reduceResults, setReduceResults] = useState<KeyValue>({})
-  const [keysSent, setKeysSent] = useState(false)
-  const [finished, setFinished] = useState(false)
-
   const [finalResults, setFinalResults] = useState<FinalResults>({
     mapTotalCount: {},
     combinerTotalCount: {},
     sizes: initialSizes,
   })
 
-  const statistics = useStatistics(finalResults)
+  const [isReadyToExecute, setIsReadyToExecute] = useState(false)
+  const [keysSent, setKeysSent] = useState(false)
+  const [finished, setFinished] = useState(false)
+  const [mapCombinerExecuted, setMapCombinerExecuted] = useState(false)
 
-  const readSizes = async () => {
-    const newSizes: Partial<Sizes> = JSON.parse((await readFile('/sizes.json')) || '')
-    newSizes.inputFiles = selectedFiles.length
-    updateSizes(newSizes)
-    return { ...finalResults.sizes, ...newSizes }
-  }
+  const { runPython, stdout, stderr, writeFile, readFile, isReady } = usePython()
+
+  const statistics = useStatistics(finalResults)
 
   const updateSizes = (newSizes: Partial<Sizes>) =>
     setFinalResults((prevResults) => ({
@@ -63,65 +56,94 @@ export default function Slave() {
       },
     }))
 
+  const readSizes = useCallback(async (): Promise<Partial<Sizes>> => {
+    let newSizes: Partial<Sizes> = JSON.parse((await readFile('/sizes.json')) || '')
+    newSizes.inputFiles = selectedFiles.length
+    return { ...newSizes }
+  }, [readFile, selectedFiles.length])
+
   const countKeyValues = (combinerResults: KeyValues) =>
     Object.keys(combinerResults).reduce((result: { [key: string]: number }, key: string) => {
       result[key] = combinerResults[key].length
       return result
     }, {})
 
-  const { runPython, stdout, stderr, writeFile, readFile, isReady } = usePython()
-
-  const runCode = async () => {
-    const readMapCombinerResults = async (): Promise<MapCombinerResults> => {
-      const mapResults: KeyValues = JSON.parse((await readFile('/map_results.txt')) || '')
-      const combinerResults: KeyValues = JSON.parse((await readFile('/combiner_results.txt')) || '')
-      return { mapResults, combinerResults }
-    }
-
-    setMapCombinerResults(initialMapCombinerResults)
-    await writeFile('/input.txt', await concatenateFiles(selectedFiles))
-    await writeFile('/map_code.py', mapReduceState.code.mapCode)
-    await writeFile('/combiner_code.py', mapReduceState.code.combinerCode)
-    await writeFile('/reduce_code.py', mapReduceState.code.reduceCode)
-    await runPython(PY_MAIN_CODE)
-    const mapCombinerResults = await readMapCombinerResults()
-    setMapCombinerResults(mapCombinerResults)
-    await readSizes()
-
-    const finalResults: Partial<FinalResults> = {
-      mapTotalCount: countKeyValues(mapCombinerResults.mapResults),
-      combinerTotalCount: countKeyValues(mapCombinerResults.combinerResults),
-    }
-    setFinalResults((prevResults) => ({ ...prevResults, ...finalResults }))
-
-    const data = {
-      type: 'MAP_COMBINER_EJECUTADO',
-      payload: {
-        combinerResults: finalResults.combinerTotalCount,
-        mapResults: finalResults.mapTotalCount,
-      },
-    }
-    sendDirectMessage(roomOwner?.userID as UserID, data)
-  }
-
   useEffect(() => {
-    // todavía no se recibió el código map a ejecutar
-    if (mapReduceState.code.mapCode == placeholdersFunctions.map.code) return
+    // This useEffect will be executed when the map code has been received and the python module is ready to execute the map phase.
+    // The map combiner fase was be executed.
+    if (mapCombinerExecuted) return
 
-    // todavía no se puede ejecutar código python
+    // the map code has not been received yet
+    if (mapReduceState.code.mapCode === placeholdersFunctions.map.code) return //? Changed to "===" ?
+
+    // yet the python code cannot be executed
     if (!isReady) return
 
+    const runCode = async () => {
+      const readMapCombinerResults = async (): Promise<MapCombinerResults> => {
+        const mapResults: KeyValues = JSON.parse((await readFile('/map_results.txt')) || '')
+        const combinerResults: KeyValues = JSON.parse(
+          (await readFile('/combiner_results.txt')) || '',
+        )
+        return { mapResults, combinerResults }
+      }
+
+      setMapCombinerResults(initialMapCombinerResults)
+      await writeFile('/input.txt', await concatenateFiles(selectedFiles))
+      await writeFile('/map_code.py', mapReduceState.code.mapCode)
+      await writeFile('/combiner_code.py', mapReduceState.code.combinerCode)
+      await writeFile('/reduce_code.py', mapReduceState.code.reduceCode)
+      await runPython(PY_MAIN_CODE)
+      const mapCombinerResults = await readMapCombinerResults()
+      setMapCombinerResults(mapCombinerResults)
+      const newSizes = await readSizes()
+      updateSizes(newSizes)
+
+      const finalResults: Partial<FinalResults> = {
+        mapTotalCount: countKeyValues(mapCombinerResults.mapResults),
+        combinerTotalCount: countKeyValues(mapCombinerResults.combinerResults),
+      }
+
+      setFinalResults((prevResults) => ({ ...prevResults, ...finalResults }))
+
+      const data = {
+        type: 'MAP_COMBINER_EJECUTADO',
+        payload: {
+          combinerResults: finalResults.combinerTotalCount,
+          mapResults: finalResults.mapTotalCount,
+        },
+      }
+      sendDirectMessage(roomOwner?.userID as UserID, data)
+    }
+
     runCode()
-  }, [mapReduceState.code, isReady])
+
+    setMapCombinerExecuted(true)
+  }, [
+    isReady,
+    mapCombinerExecuted,
+    mapReduceState.code.combinerCode,
+    mapReduceState.code.mapCode,
+    mapReduceState.code.reduceCode,
+    readFile,
+    readSizes,
+    roomOwner?.userID,
+    runPython,
+    selectedFiles,
+    sendDirectMessage,
+    writeFile,
+  ])
 
   useEffect(() => {
     // That means that the combiner has been executed. Now we can send the keys to the other users (reducers)
     if (finished) return
     if (!mapCombinerExecuted) return
+    if (keysSent) return
+
     if (!mapReduceState.sendKeys) return
+    // The node does not have any keys to send to the other nodes
     if (!Object.keys(mapReduceState.sendKeys).length) {
-      setKeysSent(true)
-      return
+      return setKeysSent(true)
     }
 
     let totalKeysSent = 0
@@ -129,10 +151,13 @@ export default function Slave() {
     let totalBytesSent = 0
     Object.entries(mapReduceState.sendKeys).forEach(([user, keys]) => {
       totalKeysSent += keys.length
-      totalValuesSent += keys.reduce((acc, key) => acc + combinerResults[key].length, 0)
+      totalValuesSent += keys.reduce(
+        (acc, key) => acc + mapCombinerResults.combinerResults[key].length,
+        0,
+      )
 
       const keysForUser: { [key: string]: unknown[] } = {}
-      keys.forEach((key) => (keysForUser[key] = combinerResults[key]))
+      keys.forEach((key) => (keysForUser[key] = mapCombinerResults.combinerResults[key]))
 
       totalBytesSent += sendDirectMessage(user as UserID, {
         type: 'RECIBIR_CLAVES',
@@ -147,12 +172,22 @@ export default function Slave() {
     })
 
     setKeysSent(true)
-  }, [combinerResults, finished, mapCombinerExecuted, sendDirectMessage, mapReduceState.sendKeys])
+  }, [
+    mapCombinerResults.combinerResults,
+    finished,
+    mapCombinerExecuted,
+    sendDirectMessage,
+    mapReduceState.sendKeys,
+    keysSent,
+  ])
 
   useEffect(() => {
-    // Thar useEffect will be executed when all the reducers (users) have sent their keys to the actual user. The map combiner has been executed.
+    // That useEffect will be executed when all the reducers (users) have sent their keys to the actual user. The map combiner has been executed.
     if (finished) return
     if (!mapCombinerExecuted) return
+    if (!keysSent) return
+    // Check if the python module is ready to execute the reduce phase.
+    if (!isReady) return
     // Check if all the keys have been received from the other users.
     if (!mapReduceState?.receiveKeysFrom) return
     if (
@@ -162,15 +197,12 @@ export default function Slave() {
       )
     )
       return
-    // Check if the python module is ready to execute the reduce phase.
-    if (!isReady) return
-
-    if (!keysSent) return
 
     //  Combine all the keys received from the other users
-    const newCombinerResults = { ...combinerResults }
+    const newCombinerResults = { ...mapCombinerResults.combinerResults }
     let totalKeysReceived = 0
     let totalValuesReceived = 0
+
     Object.values(mapReduceState.clavesRecibidas).forEach((keyList) => {
       Object.entries(keyList).forEach(([key, values]) => {
         totalKeysReceived++
@@ -178,12 +210,12 @@ export default function Slave() {
         newCombinerResults[key] = [...(newCombinerResults[key] || []), ...values]
       })
     })
-    const updatedSizes: Partial<Sizes> = {
+
+    const receivedDataSizes: Partial<Sizes> = {
       totalKeysReceived,
       totalValuesReceived,
       totalBytesReceived: mapReduceState.sizes.totalBytesReceived,
     }
-    updateSizes(updatedSizes)
 
     // Discard the keys that the user will not reduce
     const newReduceKeys: KeyValues = {}
@@ -199,7 +231,7 @@ export default function Slave() {
       const reduceResult: KeyValue = JSON.parse(data)
       setReduceResults(reduceResult)
 
-      const newSizes = await readSizes()
+      const reduceSizes = await readSizes()
 
       sendDirectMessage(roomOwner?.userID as UserID, {
         type: 'RESULTADO_FINAL',
@@ -207,19 +239,22 @@ export default function Slave() {
           reduceResult,
           sizes: {
             ...finalResults.sizes,
-            ...newSizes,
-            ...updatedSizes,
+            ...reduceSizes,
+            ...receivedDataSizes,
           },
         },
       })
+
+      return reduceSizes
     }
 
-    readResult()
+    const reduceSizes = readResult()
 
     setFinished(true)
+
+    updateSizes({ ...receivedDataSizes, ...reduceSizes })
   }, [
     keysSent,
-    combinerResults,
     finished,
     isReady,
     mapCombinerExecuted,
@@ -234,6 +269,7 @@ export default function Slave() {
     readSizes,
     sendDirectMessage,
     finalResults.sizes,
+    mapCombinerResults.combinerResults,
   ])
 
   const readyToExecute = () => {
