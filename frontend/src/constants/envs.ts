ffb2407c13b5bed03544e383a45c846cbc99a681
David Scoffield <davidscoffield01@gmail.com>
2024-03-19 19:39:58 -0300

    Add max file size and accepted file type constants

--- frontend/src/constants/envs.ts
+++ frontend/src/constants/envs.ts
@@ -2,4 +2,10 @@ export const ENVS = {
   SERVER: {
     URL: process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000',
   },
+  GENERAL: {
+    FILES: {
+      MAX_SIZE: 5 * 1024 * 1024, // 5Mb
+      ACCEPT: '.txt',
+    },
+  },
 }
