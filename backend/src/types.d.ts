de91729293c2ffd4abf6b72557a87eb59ae7ac13
Valentín Giorgetti <valentingiorgetti@hotmail.com>
2024-03-23 19:25:13 -0300

    Modifico el tipo Rooms
    Agrego un booleano para indicar si la sala está bloqueada o no

--- backend/src/types.d.ts
+++ backend/src/types.d.ts
@@ -6,12 +6,19 @@ export type Session = {
   socketConnected: boolean
   isRoomOwner: boolean
 }
+
 export type RoomID = `${string & { length: 10 }}`
 export type SessionID = UUID
 export type UserID = UUID
 
 export type RoomSessions = Map<SessionID, Session>
-export type Rooms = Map<RoomID, RoomSessions>
+
+export type Room = {
+  sessions: RoomSessions
+  locked: boolean
+}
+
+export type Rooms = Map<RoomID, Room>
 
 export type ReturningSignalParams = {
   callerID: UserID
