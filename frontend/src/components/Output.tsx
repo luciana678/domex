b36dd011503667dfea7744ba48cb4fc66e345e8b
Valentín Giorgetti <valentingiorgetti@hotmail.com>
2024-03-29 20:48:07 -0300

    Componente Output para mostrar errores y salida estándar de python

---
+++ frontend/src/components/Output.tsx
@@ -0,0 +1,17 @@
+import { Textarea } from '@mui/joy'
+import { FormLabel } from '@mui/material'
+
+export default function Output({ stdout, stderr }: { stdout: string; stderr: string }) {
+  return (
+    <div className='mt-4 flex flex-col w-[600px] gap-5'>
+      <div>
+        <FormLabel>Salida estandar</FormLabel>
+        <Textarea defaultValue={stdout} variant='soft' maxRows={5} />
+      </div>
+      <div>
+        <FormLabel>Errores</FormLabel>
+        <Textarea defaultValue={stderr} color='danger' variant='soft' maxRows={5} />
+      </div>
+    </div>
+  )
+}
