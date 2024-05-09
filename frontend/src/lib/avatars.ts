c0bfe77c0910afca90a363475fd316c398db9f32
David Scoffield <davidscoffield01@gmail.com>
2024-05-09 20:48:02 -0300

    feat: Add generateInitialsAvatar function to create avatars based on user names

---
+++ frontend/src/lib/avatars.ts
@@ -0,0 +1,14 @@
+export const generateInitialsAvatar = async (name: string) => {
+  try {
+    const res = await fetch(`https://avatar.iran.liara.run/username?username=${name}`)
+
+    if (!res.ok) throw new Error('Network response was not ok.')
+
+    const blob = await res.blob()
+
+    return URL.createObjectURL(blob)
+  } catch (err) {
+    console.error(err)
+    return null
+  }
+}
